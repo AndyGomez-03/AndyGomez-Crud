cc3a38093ba9518d4c63fc4cdb9d96b3a4ae72e8
AndyGomez-03 <91900220+AndyGomez-03@users.noreply.github.com>
2021-10-13 13:07:32 -0600

    Subida de Archivos

---
+++ PHP_CRUD_MYSQL/delete_task.php
@@ -0,0 +1,28 @@
+<?php
+
+    include("db.php");
+
+
+    if (isset($_GET['id'])){
+        $id = $_GET['id'];
+
+        $query  = "DELETE FROM task WHERE id = $id";
+        $result = mysqli_query($conn,$query);
+        if(!$result){
+            die("QUERY FAILED");
+        }
+
+
+
+
+        $_SESSION['message'] = "Task Remove Successfully";
+        $_SESSION['message_type'] = 'danger';
+        header("Location: index.php");
+    }   
+
+
+
+
+
+
+?>
